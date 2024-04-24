f311c9cfc2344f7916acba578552bc98e5325dca
SK <37695208+stevenokuto@users.noreply.github.com>
2024-04-24 19:36:00 +0800

    Update ubtu-init.sh

--- ubtu-init.sh
+++ ubtu-init.sh
@@ -19,7 +19,7 @@ sudo apt-get install linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update -y && sudo apt-get -y install nvidia-driver-545 cuda
+sudo apt-get update -y && sudo apt-get -y install nvidia-driver-550 cuda
 rm cuda-keyring_1.0-1_all.deb
 
 # search driver version
