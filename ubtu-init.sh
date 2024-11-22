87c4b6ecc8d7d116d9d863a5cbba36c0a305814d
SK <stevenokuto@gmail.com>
2024-11-22 14:09:25 +0800

    Update ubtu-init.sh
    add ncdu

--- ubtu-init.sh
+++ ubtu-init.sh
@@ -9,7 +9,7 @@ sudo sh get-docker.sh
 rm get-docker.sh
 
 # some tools
-sudo apt-get install htop tmux openssh-server smartmontools python3-pip unzip auditd gcp ffmpeg parallel -y
+sudo apt-get install htop tmux openssh-server smartmontools python3-pip unzip auditd gcp ffmpeg parallel ncdu -y
 
 # Rust language (this will block the script)
 # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
